63bf55b4c5f9e01f2074e7f17c10004f178d1ca3
Richard Ogin <rogin@users.noreply.github.com>
2023-01-12 22:21:07 -0600

    add mirth logout

--- PS_Mirth.psm1
+++ PS_Mirth.psm1
@@ -6841,6 +6841,49 @@ function Connect-Mirth {
 
 }  # Connect-Mirth
 
+function Disconnect-Mirth { 
+    [CmdletBinding()]
+    PARAM (
+        # A MirthConnection is required. You can obtain one from Connect-Mirth.
+        [Parameter(ValueFromPipeline = $True)]
+        [MirthConnection]$connection = $currentConnection
+    )
+    BEGIN {
+        Write-Debug "Disconnect-Mirth Beginning..."
+    }
+    PROCESS {
+        if ($null -eq $connection) { 
+            Throw "You must first obtain a MirthConnection by invoking Connect-Mirth" 
+        }          
+        [Microsoft.PowerShell.Commands.WebRequestSession]$session = $connection.session
+        $serverUrl = $connection.serverUrl
+
+        Write-Debug "Disconnecting from Mirth..."
+        $headers = $DEFAULT_HEADERS.Clone()
+        $headers.Add("accept", "application/xml")
+        $headers.Add("Content-Type", "application/xml")
+        $uri = $serverUrl + '/api/users/_logout'
+
+        Write-Debug "Invoking POST Mirth $uri"
+        Write-Debug "`$headers=$($headers.GetEnumerator())"
+        try { 
+            Invoke-RestMethod -uri $uri -Headers $headers -Method Post -WebSession $session -StatusCodeVariable statusCode | Out-Null
+            
+            #expect statusCode=204
+            Write-Debug "`$statusCode=$statusCode"
+
+            $script:currentConnection = $null
+        }
+        catch {
+            Write-Error $_
+        }
+    }
+    END {
+        Write-Debug "Disconnect-Mirth Ending>..."
+    }
+
+}  # Disconnect-Mirth
+
 function Set-MirthUserPassword {
     <#
     .SYNOPSIS
